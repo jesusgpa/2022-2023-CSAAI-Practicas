cd272e6c57dd52312701f43fa50ff635b54db0be
jesusgpa <jesus.parrado@urjc.es>
2023-01-26 10:16:15 +0100

    limipieza

--- P2/S4/Ej-04.js
+++
@@ -1,3 +0,0 @@
-console.log("La página ya está cargada")
-console.log("Ahora es seguro ejecutar el código js")
-console.log("defer....")
--- P2/S4/plantilla.js
+++
@@ -1,4 +0,0 @@
-
-//-- Punto de entrada: una vez cargada la página se llama a esta
-console.log("Aquí comienza tu código JS...")
-console.log("¡Que comiencen los juegos del JS!")

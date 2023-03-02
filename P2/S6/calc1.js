020e5cd51204e69e2e2aedb3bf9a040e75ab442e
jesusgpa <jesus.parrado@urjc.es>
2023-03-02 10:44:38 +0100

    maquina de estados

--- P2/S6/calc1.js
+++ P2/S6/calc1.js
@@ -1,6 +1,7 @@
 console.log("Ejecutando JS...");
 
 
+
 //-- Elementos de la interfaz de la calculadora
 display = document.getElementById("display")
 boton1 = document.getElementById("boton1")
--- P2/S6/calc4.js
+++ P2/S6/calc4.js
@@ -1,8 +1,42 @@
 console.log("Ejecutando JS...");
 
+//-- Estados posibles de la calculadora
+const ESTADO = {
+    INIT: 0,
+    OP1: 1,
+    OPERATION: 2,
+    OP2: 3,
+  }
+
+//-- Variable de estado
+//-- Por defecto su valor será el del estado inicial
+let estado = ESTADO.INIT;
+
 display = document.getElementById("display")
 igual = document.getElementById("igual")
 clear = document.getElementById("clear")
+
+//-- Función de retrollamada de los digitos
+function digito( ev ) {
+
+    
+    if ( estado == ESTADO.INIT ) {
+
+        display.innerHTML = ev.target.value;
+
+        //-- Pasar al siguiente estado
+        estado = ESTADO.OP1;
+
+    } else {
+
+        //--En cualquier otro estado lo añadimos
+        display.innerHTML += ev.target.value;
+
+    }
+
+
+}
+
 
 //-- Obtener una colección con todos los elementos
 //-- de la clase dígito
@@ -13,11 +47,11 @@ digitos = document.getElementsByClassName("digito")
 for (let boton of digitos) {
 
     //-- Se ejecuta cuando se pulsa un boton
-    //-- que es un dígito
-    boton.onclick = (ev) => {
-        display.innerHTML += ev.target.value;
-        console.log("DIGITO!!!");
-    }
+    //-- que es un dígito. Para que el código sea 
+    //-- mas legible la función de retrollamada se
+    //-- escribe como una función normal (digito)
+    boton.onclick = digito;
+
 }
 
 //-- Establecer la misma función de retrollamada
